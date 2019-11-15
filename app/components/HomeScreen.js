ac47e11d4a5bf7785ccae43cbe2aa355090adcb1
Kaitlyn Martinez <kkmartinez95@gmail.com>
2019-11-14 20:54:24 -0500

    bkGround

--- app/components/HomeScreen.js
+++ app/components/HomeScreen.js
@@ -1,20 +1,43 @@
 import React, { Component } from "react";
-import { Button } from "react-native";
+import { Button, StyleSheet, View, ImageBackground } from "react-native";
+import { BackGround } from "../../public/BackGround.jpg";
+
 // import { AppRegistry, Text, View, TextInput } from "react-native";
 
 class HomeScreen extends Component {
   static navigationOptions = {
-    title: "Welcome"
+    title: "PineApple"
   };
   render() {
     const { navigate } = this.props.navigation;
     return (
-      <Button
-        title="Go to Jane's profile"
-        onPress={() => navigate("Profile", { name: "Jane" })}
-      />
+      <ImageBackground
+        style={styles.title}
+        source={require("../../public/BackGround.jpg")}
+      >
+        <View>
+          <Button
+            title="Go to Jane's profile"
+            onPress={() => navigate("Profile", { name: "Jane" })}
+          />
+        </View>
+      </ImageBackground>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  title: {
+    // backgroundColor: "#F9EBA5",
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+  // button: {
+  //   backgroundColor: "#2D898B",
+  //   fontSize: 40,
+  //   fontWeight: "bold"
+  // },
+});
 
 export default HomeScreen;
