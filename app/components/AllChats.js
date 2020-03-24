ff44e44b07256bb94f3521efcf3851338a06d037
Kaitlyn Martinez <kkmartinez95@gmail.com>
2020-03-24 16:58:11 -0400

    idk what is going on....

---
+++ app/components/AllChats.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+import styles from "../../public/styles";
+
+class AllChats extends Component {
+  render() {
+    return (
+      <View style={styles.loginPage}>
+        <Text>HI from ALL CHATS</Text>
+      </View>
+    );
+  }
+}
+
+export default AllChats;
